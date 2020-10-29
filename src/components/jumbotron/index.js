941a26a293ac9c5e58b6c7236528aab97ddab6d8
Lincer6 <Dober-01@yandex.ua>
2020-10-29 20:21:18 +0200

    Jumbotron made

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,42 @@
+import React from "react"
+import {Container, Image, Inner, Item, Pane, SubTitle, Title} from "./styles/jumbotron";
+
+export const Jumbotron = ({ children, direction = 'row', ...restProps }) => {
+    return (
+        <Item { ...restProps }>
+            <Inner direction={direction}>
+                {children}
+            </Inner>
+        </Item>
+    )
+}
+
+Jumbotron.Pane = ({ children, ...restProps }) => {
+    return (
+        <Pane { ...restProps }>{children}</Pane>
+    )
+}
+
+Jumbotron.Container = ({ children, ...restProps }) => {
+    return (
+        <Container { ...restProps }>{children}</Container>
+    )
+}
+
+Jumbotron.Title = ({ children, ...restProps }) => {
+    return (
+        <Title { ...restProps }>{children}</Title>
+    )
+}
+
+Jumbotron.SubTitle = ({ children, ...restProps }) => {
+    return (
+        <SubTitle { ...restProps }>{children}</SubTitle>
+    )
+}
+
+Jumbotron.Image = (props) => {
+    return (
+        <Image { ...props }/>
+    )
+}
